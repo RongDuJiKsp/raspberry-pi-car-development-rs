544db0236466d376ad0144a48fe08613e23ecd5d
YuQi Zhou <rdjksp@gmail.com>
2025-01-07 22:12:18 +0800

    init

---
+++ src/mlib/lib_pins.rs
@@ -0,0 +1,19 @@
+pub mod pins_wiringpi {
+    pub const BEEP_PIN: u8 = 0;
+    pub const POW_PWM_L_PIN: u8 = 1;
+    pub const POW_IN2_L_PIN: u8 = 2;
+    pub const POW_IN1_L_PIN: u8 = 3;
+    pub const POW_PWM_R_PIN: u8 = 4;
+    pub const POW_IN2_R_PIN: u8 = 5;
+    pub const POW_IN1_R_PIN: u8 = 6;
+    pub const BTN_PIN: u8 = 7;
+    pub const RED_PIN: u8 = 21;
+    pub const GREEN_PIN: u8 = 22;
+    pub const LEFT_TCR_PIN: u8 = 25;
+    pub const MID_TCR_PIN: u8 = 24;
+    pub const RIGHT_TCR_PIN: u8 = 23;
+    pub const LEFT_OA_PIN: u8 = 26;
+    pub const RIGHT_OA_PIN: u8 = 27;
+    pub const TRIG_SEND: u8 = 28;
+    pub const TRIG_RECV: u8 = 29;
+}
---
+++ src/mlib/mod.rs
@@ -0,0 +1,2 @@
+pub mod lib_alias;
+pub mod lib_pins;
